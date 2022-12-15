4e71524675fd5104740f8458c491b7f075bfa7b1
Sam MacPherson <xylom44@gmail.com>
2022-12-15 12:48:41 -0500

    e-mode works; fix giving tokens to stETH

--- src/test/IntegrationTest.t.sol
+++ src/test/IntegrationTest.t.sol
@@ -14,6 +14,10 @@ import {AaveOracle} from 'aave-v3-core/contracts/misc/AaveOracle.sol';
 import {DataTypes} from "aave-v3-core/contracts/protocol/libraries/types/DataTypes.sol";
 import {ReserveConfiguration} from "aave-v3-core/contracts/protocol/libraries/configuration/ReserveConfiguration.sol";
 
+interface stETHLike {
+    function getTotalShares() external view returns (uint256);
+}
+
 contract User {
 
     Pool public pool;
@@ -46,12 +50,30 @@ contract IntegrationTest is Test {
     User[] users;
     address[] assets;
 
+    // TODO these should not be hard coded
+    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 stETH = IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
+    IERC20 wbtc = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+
+    function _divup(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        unchecked {
+            z = x != 0 ? ((x - 1) / y) + 1 : 0;
+        }
+    }
+
     function giveTokens(address token, uint256 amount) internal {
         // Edge case - balance is already set for some reason
         if (IERC20(token).balanceOf(address(this)) == amount) return;
 
+        // Special exceptions for rebase tokens
+        uint256 convertedAmount = amount;
+        if (token == 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84) {
+            convertedAmount = _divup(amount * stETHLike(token).getTotalShares(), IERC20(token).totalSupply());
+        }
+
         // Scan the storage for the balance storage slot
-        for (uint256 i = 0; i < 200; i++) {
+        for (uint256 i = 0; i < 20; i++) {
             // Solidity-style storage layout for maps
             {
                 bytes32 prevValue = vm.load(
@@ -62,7 +84,7 @@ contract IntegrationTest is Test {
                 vm.store(
                     address(token),
                     keccak256(abi.encode(address(this), uint256(i))),
-                    bytes32(amount)
+                    bytes32(convertedAmount)
                 );
                 if (IERC20(token).balanceOf(address(this)) == amount) {
                     // Found it
@@ -72,31 +94,6 @@ contract IntegrationTest is Test {
                     vm.store(
                         address(token),
                         keccak256(abi.encode(address(this), uint256(i))),
-                        prevValue
-                    );
-                }
-            }
-
-            // Vyper-style storage layout for maps
-            {
-                bytes32 prevValue = vm.load(
-                    address(token),
-                    keccak256(abi.encode(uint256(i), address(this)))
-                );
-
-                vm.store(
-                    address(token),
-                    keccak256(abi.encode(uint256(i), address(this))),
-                    bytes32(amount)
-                );
-                if (IERC20(token).balanceOf(address(this)) == amount) {
-                    // Found it
-                    return;
-                } else {
-                    // Keep going after restoring the original value
-                    vm.store(
-                        address(token),
-                        keccak256(abi.encode(uint256(i), address(this))),
                         prevValue
                     );
                 }
@@ -123,15 +120,13 @@ contract IntegrationTest is Test {
         for (uint256 i = 0; i < assets.length; i++) {
             address asset = assets[i];
             uint256 numTokens = valuePerAsset * (10 ** IERC20Detailed(asset).decimals()) * aaveOracle.BASE_CURRENCY_UNIT() / aaveOracle.getAssetPrice(asset);
-            if (i != 2) {   // Skip Lido for now stETH doesn't work
-                for (uint256 o = 0; o < users.length; o++) {
-                    giveTokens(asset, numTokens);
-                    IERC20(asset).transfer(address(users[o]), numTokens);
-                }
-
-                // Have the first user seed all pools
-                users[0].supply(IERC20(asset), numTokens);
+            for (uint256 o = 0; o < users.length; o++) {
+                giveTokens(asset, numTokens);
+                IERC20(asset).transfer(address(users[o]), numTokens);
             }
+
+            // Have the third user seed all pools
+            users[2].supply(IERC20(asset), numTokens);
         }
     }
 
@@ -141,9 +136,9 @@ contract IntegrationTest is Test {
     }
 
     function test_borrow() public {
-        User user = users[1];
-        IERC20 supplyAsset = IERC20(assets[1]);
-        IERC20 borrowAsset = IERC20(assets[0]);
+        User user = users[0];
+        IERC20 supplyAsset = weth;
+        IERC20 borrowAsset = dai;
         uint256 collateralAmount = supplyAsset.balanceOf(address(user));
 
         user.supply(supplyAsset, collateralAmount);
@@ -151,9 +146,10 @@ contract IntegrationTest is Test {
     }
 
     function test_emode() public {
-        //user2.supply(stETH, 100 ether);
-        //user2.setEMode(1);
-        //user2.borrow(weth, 95 ether);   // Should be able to borrow up to 95%
+        User user = users[0];
+        user.setEMode(1);
+        user.supply(stETH, 10 ether);
+        user.borrow(weth, 8.5 ether);   // Should be able to borrow up to 85%
     }
 
 }
--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ fi
 source ".env-deploy"
 
 if [[ -z "$1" ]]; then
-  forge test --rpc-url="$ETH_RPC_URL"
+    forge test --rpc-url="$ETH_RPC_URL"
 else
-  forge test --rpc-url="$ETH_RPC_URL" --match "$1" -vvvv
+    forge test --rpc-url="$ETH_RPC_URL" --match "$1" -vvvv
 fi
