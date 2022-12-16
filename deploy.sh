abdf131083f9693980826b54d4e617248d61a2ee
Sam MacPherson <xylom44@gmail.com>
2022-12-16 16:56:07 -0500

    getting things to deploy again

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,14 @@
 set -e
 
 echo "Deploying Aave contracts..."
-if [[ -z "$ETH_PASSWORD" ]]; then
-    forge script script/DeployAave.s.sol:DeployAave --use solc:0.8.10 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast | tee out/tmp.out
-else
-    PASS=$(cat $ETH_PASSWORD)
+forge script script/DeployAave.s.sol:DeployAave --use solc:0.8.10 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast
+loaddeploy broadcast/DeployAave.s.sol/1/run-latest.json
 
-    forge script script/DeployAave.s.sol:DeployAave --use solc:0.8.10 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast --password "$PASS" | tee out/tmp.out
-fi
+echo "Deploying D3M contracts..."
 
-if test -f ".env-deploy"; then
-    rm .env-deploy
-fi
-for i in $(grep "LENDING_POOL_ADDRESS_PROVIDER=" -A 5 <(cat "out/tmp.out")); do
-    echo "export $i" >> .env-deploy
-done
-source .env-deploy
+export DEPLOY_D3M_TYPE="aave"
+export DEPLOY_ADMIN="$ETH_FROM"
+export DEPLOY_ILK="DIRECT-SPARK-DAI"
+export DEPLOY_AAVE_LENDING_POOL="$DEPLOY_Pool"
+
+forge script dependencies/dss-direct-deposit/script/DeployD3M.s.sol:DeployD3M --use solc:0.8.14 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast
--- script/DeployAave.s.sol
+++ script/DeployAave.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pragma solidity ^0.8.10;
+pragma solidity 0.8.10;
 
 import "forge-std/Script.sol";
 import {stdJson} from "forge-std/StdJson.sol";
