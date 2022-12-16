041fd85b9e6fd43acc0709dc4cba1dd2a27e4ebb
Sam MacPherson <xylom44@gmail.com>
2022-12-16 17:12:32 -0500

    got d3m compiling

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@ set -e
 
 echo "Deploying Aave contracts..."
 forge script script/DeployAave.s.sol:DeployAave --use solc:0.8.10 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast
-loaddeploy broadcast/DeployAave.s.sol/1/run-latest.json
+
+for s in $( jq -r ".transactions|to_entries|map_values(\"\(.value.contractName)=\(.value.contractAddress)\")|.[]" broadcast/DeployAave.s.sol/1/run-latest.json ); do
+    export "DEPLOY_$s"
+done
 
 echo "Deploying D3M contracts..."
 
@@ -12,4 +15,6 @@ export DEPLOY_ADMIN="$ETH_FROM"
 export DEPLOY_ILK="DIRECT-SPARK-DAI"
 export DEPLOY_AAVE_LENDING_POOL="$DEPLOY_Pool"
 
-forge script dependencies/dss-direct-deposit/script/DeployD3M.s.sol:DeployD3M --use solc:0.8.14 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast
+cd dependencies/dss-direct-deposit
+forge script script/DeployD3M.s.sol:DeployD3M --use solc:0.8.14 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast
+cd ..
