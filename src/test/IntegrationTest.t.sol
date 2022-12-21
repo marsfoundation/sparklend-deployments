e43cc047c536fcdb6e93f7d956f134370ab34b87
Sam MacPherson <xylom44@gmail.com>
2022-12-21 09:45:26 -0500

    dont use hardcoded addresses

--- src/test/IntegrationTest.t.sol
+++ src/test/IntegrationTest.t.sol
@@ -3,6 +3,7 @@
 pragma solidity 0.8.10;
 
 import "dss-test/DSSTest.sol";
+import {stdJson} from "forge-std/StdJson.sol";
 
 import {IERC20Detailed} from 'aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20Detailed.sol';
 import {IERC20} from 'aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
@@ -47,6 +48,12 @@ contract User {
 
 contract IntegrationTest is DSSTest {
 
+    using stdJson for string;
+    using MCD for *;
+
+    string config;
+    DssInstance dss;
+
     PoolAddressesProvider poolAddressesProvider;
     Pool pool;
     AaveOracle aaveOracle;
@@ -54,62 +61,22 @@ contract IntegrationTest is DSSTest {
     User[] users;
     address[] assets;
 
-    // TODO these should not be hard coded
-    D3MHubLike hub = D3MHubLike(0x12F36cdEA3A28C35aC8C6Cc71D9265c17C74A27F);
-    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IERC20 stETH = IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
-    IERC20 wbtc = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
-    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-
-    function _divup(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        unchecked {
-            z = x != 0 ? ((x - 1) / y) + 1 : 0;
-        }
-    }
-
-    function giveTokens(address token, uint256 amount) internal {
-        // Edge case - balance is already set for some reason
-        if (IERC20(token).balanceOf(address(this)) == amount) return;
-
-        // Special exceptions for rebase tokens
-        uint256 convertedAmount = amount;
-        if (token == 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84) {
-            convertedAmount = _divup(amount * stETHLike(token).getTotalShares(), IERC20(token).totalSupply());
-        }
-
-        // Scan the storage for the balance storage slot
-        for (uint256 i = 0; i < 20; i++) {
-            // Solidity-style storage layout for maps
-            {
-                bytes32 prevValue = vm.load(
-                    address(token),
-                    keccak256(abi.encode(address(this), uint256(i)))
-                );
-
-                vm.store(
-                    address(token),
-                    keccak256(abi.encode(address(this), uint256(i))),
-                    bytes32(convertedAmount)
-                );
-                if (IERC20(token).balanceOf(address(this)) == amount) {
-                    // Found it
-                    return;
-                } else {
-                    // Keep going after restoring the original value
-                    vm.store(
-                        address(token),
-                        keccak256(abi.encode(address(this), uint256(i))),
-                        prevValue
-                    );
-                }
-            }
-        }
-
-        // We have failed if we reach here
-        require(false, "giveTokens-slot-not-found");
-    }
+    D3MHubLike hub;
+    IERC20 weth;
+    IERC20 stETH;
+    IERC20 wbtc;
+    IERC20 dai;
 
     function setUp() public {
+        config = ScriptTools.readInput("config");
+        dss = MCD.loadFromChainlog(config.readAddress(".chainlog"));
+
+        hub = D3MHubLike(dss.chainlog.getAddress("DIRECT_HUB"));
+        weth = IERC20(dss.chainlog.getAddress("ETH"));
+        stETH = IERC20(dss.chainlog.getAddress("STETH"));
+        wbtc = IERC20(dss.chainlog.getAddress("WBTC"));
+        dai = IERC20(dss.chainlog.getAddress("MCD_DAI"));
+
         poolAddressesProvider = PoolAddressesProvider(ScriptTools.importContract("LENDING_POOL_ADDRESS_PROVIDER"));
         pool = Pool(poolAddressesProvider.getPool());
         aaveOracle = AaveOracle(poolAddressesProvider.getPriceOracle());
@@ -169,6 +136,54 @@ contract IntegrationTest is DSSTest {
         user.setEMode(1);
         user.supply(stETH, 10 ether);
         user.borrow(weth, 8.5 ether);   // Should be able to borrow up to 85%
+    }
+
+    function _divup(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        unchecked {
+            z = x != 0 ? ((x - 1) / y) + 1 : 0;
+        }
+    }
+
+    function giveTokens(address token, uint256 amount) internal {
+        // Edge case - balance is already set for some reason
+        if (IERC20(token).balanceOf(address(this)) == amount) return;
+
+        // Special exceptions for rebase tokens
+        uint256 convertedAmount = amount;
+        if (token == address(stETH)) {
+            convertedAmount = _divup(amount * stETHLike(token).getTotalShares(), IERC20(token).totalSupply());
+        }
+
+        // Scan the storage for the balance storage slot
+        for (uint256 i = 0; i < 20; i++) {
+            // Solidity-style storage layout for maps
+            {
+                bytes32 prevValue = vm.load(
+                    address(token),
+                    keccak256(abi.encode(address(this), uint256(i)))
+                );
+
+                vm.store(
+                    address(token),
+                    keccak256(abi.encode(address(this), uint256(i))),
+                    bytes32(convertedAmount)
+                );
+                if (IERC20(token).balanceOf(address(this)) == amount) {
+                    // Found it
+                    return;
+                } else {
+                    // Keep going after restoring the original value
+                    vm.store(
+                        address(token),
+                        keccak256(abi.encode(address(this), uint256(i))),
+                        prevValue
+                    );
+                }
+            }
+        }
+
+        // We have failed if we reach here
+        require(false, "giveTokens-slot-not-found");
     }
 
 }
