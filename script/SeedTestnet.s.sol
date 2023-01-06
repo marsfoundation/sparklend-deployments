7346080714e862370dc5b291ef5419712d981607
Sam MacPherson <xylom44@gmail.com>
2023-01-06 16:50:24 -0600

    got seeding working

--- script/SeedTestnet.s.sol
+++ script/SeedTestnet.s.sol
@@ -15,8 +15,10 @@ import { MintableERC20 } from "aave-v3-core/contracts/mocks/tokens/MintableERC20
 import { IUniswapV3Factory } from "v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import { IUniswapV3Pool } from "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
+import { TickMath } from "../src/TickMath.sol";
 import { DaiFaucet } from "../src/DaiFaucet.sol";
 import { LiquidityAmounts } from "../src/LiquidityAmounts.sol";
+import { INonfungiblePositionManager } from "../src/INonfungiblePositionManager.sol";
 
 contract SeedTestnet is Script {
 
@@ -26,18 +28,26 @@ contract SeedTestnet is Script {
     string config;
     DssInstance dss;
 
+    address deployer;
+
     PoolAddressesProvider poolAddressesProvider;
     Pool pool;
     AaveOracle oracle;
 
-    IUniswapV3Factory factory;
+    DaiFaucet daiFaucet;
 
+    IUniswapV3Factory factory;
+    INonfungiblePositionManager manager;
     address[] tokens;
     address tokenA;
     address tokenB;
     uint24 fee;
     address token0;
     address token1;
+    uint256 tokensToMint;
+    address upool;
+    uint256 perTokenDai;
+    uint160 sqrtPriceX96;
 
     function run() external {
         config = ScriptTools.readInput("config");
@@ -47,12 +57,13 @@ contract SeedTestnet is Script {
         pool = Pool(ScriptTools.importContract("LENDING_POOL"));
         oracle = AaveOracle(poolAddressesProvider.getPriceOracle());
         factory = IUniswapV3Factory(vm.envAddress("UNISWAP_V3_FACTORY"));
+        manager = INonfungiblePositionManager(vm.envAddress("UNISWAP_V3_POSITION_MANAGER"));
 
-        address deployer = msg.sender;
+        deployer = msg.sender;
 
         vm.startBroadcast();
         // Add some faucet DAI to the pool to simulate a D3M deposit
-        DaiFaucet daiFaucet = new DaiFaucet(dss.chainlog.getAddress("MCD_PSM_USDC_A"), dss.chainlog.getAddress("FAUCET"));
+        daiFaucet = new DaiFaucet(dss.chainlog.getAddress("MCD_PSM_USDC_A"), dss.chainlog.getAddress("FAUCET"));
         daiFaucet.gulp(deployer, 1);
         dss.dai.approve(address(pool), type(uint256).max);
         pool.supply(address(dss.dai), dss.dai.balanceOf(deployer) / 2, deployer, 0);    // Only supply half to pool (other half goes to Uni V3 pools)
@@ -61,31 +72,70 @@ contract SeedTestnet is Script {
         tokens = pool.getReservesList();
         tokenB = address(dss.dai);
         fee = 500;
-        uint256 perTokenDai = dss.dai.balanceOf(deployer) / (tokens.length - 1);
+        perTokenDai = dss.dai.balanceOf(deployer) / (tokens.length - 1);
         for (uint256 i = 0; i < tokens.length; i++) {
             tokenA = tokens[i];
             if (tokenA == address(dss.dai)) continue;
 
-            // Create the pool if it doesn't exist
+            // Create the pool if it doesn't exist and initialize
             (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-            address upool = factory.getPool(token0, token1, fee);
+            upool = factory.getPool(token0, token1, fee);
+            sqrtPriceX96;
             if (upool == address(0)) {
                 upool = factory.createPool(token0, token1, fee);
+                // TODO needs to take decimals into account for WBTC
+                sqrtPriceX96 = uint160(sqrt(oracle.getAssetPrice(tokenB) * (1 << 96) / oracle.getAssetPrice(tokenA)) << 48);
+                IUniswapV3Pool(upool).initialize(sqrtPriceX96);
+            } else {
+                (sqrtPriceX96,,,,,,) = IUniswapV3Pool(upool).slot0();
             }
 
             // Add some liquidity to the pool
-            uint256 tokensToMint = perTokenDai * oracle.getAssetPrice(tokenB) * 100 / oracle.getAssetPrice(tokenA);
+            tokensToMint = perTokenDai * oracle.getAssetPrice(tokenB) * 100 / oracle.getAssetPrice(tokenA);
             MintableERC20(tokenA).mint(deployer, tokensToMint);
-            (uint160 sqrtRatioX96, , , , , , ) = IUniswapV3Pool(upool).slot0();
-            IUniswapV3Pool(upool).mint(deployer, -887272, 887272, LiquidityAmounts.getLiquidityForAmounts(
-                sqrtRatioX96,
-                4295128739,
-                1461446703485210103287273052203988822378723970342,
-                tokenA == token0 ? tokensToMint : perTokenDai,
-                tokenA == token0 ? perTokenDai : tokensToMint
-            ), "");
+            MintableERC20(tokenA).approve(address(manager), type(uint256).max);
+            MintableERC20(tokenB).approve(address(manager), type(uint256).max);
+            manager.mint(
+                INonfungiblePositionManager.MintParams({
+                    token0: token0,
+                    token1: token1,
+                    fee: fee,
+                    tickLower: TickMath.MIN_TICK + 2,
+                    tickUpper: TickMath.MAX_TICK - 2,
+                    amount0Desired: tokenA == token0 ? tokensToMint : perTokenDai,
+                    amount1Desired: tokenA == token0 ? perTokenDai : tokensToMint,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    recipient: deployer,
+                    deadline: block.timestamp + 1 hours
+                })
+            );
         }
         vm.stopBroadcast();
+    }
+
+    function sqrt(uint256 _x) private pure returns (uint128) {
+        if (_x == 0) return 0;
+        else {
+            uint256 xx = _x;
+            uint256 r = 1;
+            if (xx >= 0x100000000000000000000000000000000) { xx >>= 128; r <<= 64; }
+            if (xx >= 0x10000000000000000) { xx >>= 64; r <<= 32; }
+            if (xx >= 0x100000000) { xx >>= 32; r <<= 16; }
+            if (xx >= 0x10000) { xx >>= 16; r <<= 8; }
+            if (xx >= 0x100) { xx >>= 8; r <<= 4; }
+            if (xx >= 0x10) { xx >>= 4; r <<= 2; }
+            if (xx >= 0x8) { r <<= 1; }
+            r = (r + _x / r) >> 1;
+            r = (r + _x / r) >> 1;
+            r = (r + _x / r) >> 1;
+            r = (r + _x / r) >> 1;
+            r = (r + _x / r) >> 1;
+            r = (r + _x / r) >> 1;
+            r = (r + _x / r) >> 1; // Seven iterations should be enough
+            uint256 r1 = _x / r;
+            return uint128 (r < r1 ? r : r1);
+        }
     }
 
 }
--- seed-testnet.sh
+++ seed-testnet.sh
@@ -5,6 +5,7 @@ set -e
 source out/contract-exports.env
 
 export UNISWAP_V3_FACTORY="0x1F98431c8aD98523631AE4a59f267346ea31F984"
+export UNISWAP_V3_POSITION_MANAGER="0xC36442b4a4522E871399CD717aBDD847Ab11FE88"
 
 echo "Seeding DAI pool..."   
 forge script script/SeedTestnet.s.sol:SeedTestnet --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast
