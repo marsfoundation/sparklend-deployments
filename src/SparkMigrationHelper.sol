2ad28f82f9ac9b2e71254f25ed7fe987815d09aa
Sam MacPherson <xylom44@gmail.com>
2023-04-07 16:40:22 +0100

    finish migration support modifications and add support for USDC -> DAI swaps

--- src/SparkMigrationHelper.sol
+++ src/SparkMigrationHelper.sol
@@ -10,6 +10,12 @@ import {AaveV2Ethereum} from 'aave-address-book/AaveV2Ethereum.sol';
 import {IWstETH} from 'V2-V3-migration-helpers/src/interfaces/IWstETH.sol';
 import {MigrationHelper} from 'V2-V3-migration-helpers/src/contracts/MigrationHelper.sol';
 
+interface PsmLike {
+    function gemJoin() external view returns (address);
+    function tin() external view returns (uint256);
+    function sellGem(address usr, uint256 gemAmt) external;
+}
+
 contract SparkMigrationHelper is MigrationHelper {
   using SafeERC20 for IERC20WithPermit;
   using SafeERC20 for IWstETH;
@@ -18,10 +24,14 @@ contract SparkMigrationHelper is MigrationHelper {
     IERC20WithPermit(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
   IWstETH public constant WSTETH = IWstETH(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
   IPool public constant SPARK_POOL = IPool(0xC13e21B648A5Ee794902342038FF3aDAB66BE987);
+  IERC20WithPermit public constant DAI = IERC20WithPermit(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+  IERC20WithPermit public constant USDC = IERC20WithPermit(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+  PsmLike public constant PSM = PsmLike(0x89B78CfA322F6C5dE0aBcEecab66Aee45393cC5A);
 
   constructor() MigrationHelper(SPARK_POOL, AaveV2Ethereum.POOL) {
-    STETH.safeApprove(address(WSTETH), type(uint256).max);
-    WSTETH.safeApprove(address(SPARK_POOL), type(uint256).max);
+        STETH.safeApprove(address(WSTETH), type(uint256).max);
+        WSTETH.safeApprove(address(SPARK_POOL), type(uint256).max);
+        USDC.safeApprove(PSM.gemJoin(), type(uint256).max);
   }
 
   /// @inheritdoc MigrationHelper
@@ -30,9 +40,13 @@ contract SparkMigrationHelper is MigrationHelper {
     uint256 amount
   ) external view override returns (address, uint256) {
     if (asset == address(STETH)) {
-      uint256 wrappedAmount = WSTETH.getWstETHByStETH(amount);
+        uint256 wrappedAmount = WSTETH.getWstETHByStETH(amount);
 
-      return (address(WSTETH), wrappedAmount);
+        return (address(WSTETH), wrappedAmount);
+    } else if (asset == address(USDC)) {
+        uint256 swappedAmount = amount * 1e12;
+
+        return (address(DAI), swappedAmount - swappedAmount * PSM.tin() / 1e18);
     }
 
     return (asset, amount);
@@ -41,9 +55,13 @@ contract SparkMigrationHelper is MigrationHelper {
   /// @dev stETH is being wrapped to supply wstETH to the v3 pool
   function _preSupply(address asset, uint256 amount) internal override returns (address, uint256) {
     if (asset == address(STETH)) {
-      uint256 wrappedAmount = WSTETH.wrap(amount);
+        uint256 wrappedAmount = WSTETH.wrap(amount);
 
-      return (address(WSTETH), wrappedAmount);
+        return (address(WSTETH), wrappedAmount);
+    } else if (asset == address(USDC)) {
+        PSM.sellGem(address(this), amount);
+
+        return (address(DAI), DAI.balanceOf(address(this)));
     }
 
     return (asset, amount);
