b45e74c7515e3580aabcd3c1f95e876101601a2b
Sam MacPherson <xylom44@gmail.com>
2023-05-25 10:51:36 -0400

    started on rETH spell

---
+++ src/proposals/20230525/SparkEthereum_20230525.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {AaveV3PayloadEthereum, IEngine, Rates, EngineFlags} from 'aave-helpers/v3-config-engine/AaveV3PayloadEthereum.sol';
+
+/**
+ * @title List rETH on SparkEthereum
+ * @author Phoenix Labs
+ * @dev This proposal lists rETH on Spark Ethereum
+ * Forum: https://forum.makerdao.com/t/2023-05-24-spark-protocol-updates/20958
+ * Vote: TODO
+ */
+contract SparkEthereum_20230525 is AaveV3PayloadEthereum {
+
+    address public constant RETH = 0x553303d460EE0afB37EdFf9bE42922D8FF63220e;
+    address public constant RETH_PRICE_FEED = 0x553303d460EE0afB37EdFf9bE42922D8FF63220e;
+
+    function newListings() public pure override returns (IEngine.Listing[] memory) {
+        IEngine.Listing[] memory listings = new IEngine.Listing[](1);
+
+        listings[0] = IEngine.Listing({
+            asset: 0xae78736cd615f374d3085123a210448e74fc6393,
+            assetSymbol: 'rETH',
+            priceFeed: RETH_PRICE_FEED,
+            rateStrategyParams: Rates.RateStrategyParams({
+                optimalUsageRatio: _bpsToRay(45_00),
+                baseVariableBorrowRate: 0,
+                variableRateSlope1: _bpsToRay(7_00),
+                variableRateSlope2: _bpsToRay(300_00),
+                stableRateSlope1: 0,
+                stableRateSlope2: 0,
+                baseStableRateOffset: 0,
+                stableRateExcessOffset: 0,
+                optimalStableToTotalDebtRatio: 0
+            }),
+            enabledToBorrow: EngineFlags.ENABLED,
+            stableRateModeEnabled: EngineFlags.DISABLED,
+            borrowableInIsolation: EngineFlags.DISABLED,
+            withSiloedBorrowing: EngineFlags.DISABLED,
+            flashloanable: EngineFlags.ENABLED,
+            ltv: 68_50,
+            liqThreshold: 79_50,
+            liqBonus: 7_00,
+            reserveFactor: 15_00,
+            supplyCap: 20_000,
+            borrowCap: 2_400,
+            debtCeiling: 0,
+            liqProtocolFee: 10_00,
+            eModeCategory: 1
+        });
+
+        return listings;
+    }
+
+}
---
+++ src/proposals/20230525/SparkEthereum_20230525.t.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import 'forge-std/Test.sol';
+import {ProtocolV3_0_1TestBase, InterestStrategyValues, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
+import {SparkEthereum_20230525} from './SparkEthereum_20230525.sol';
+
+contract SparkEthereum_20230525Test is ProtocolV3_0_1TestBase, TestWithExecutor {
+    uint256 internal constant RAY = 1e27;
+    SparkEthereum_20230525 public payload;
+
+    address internal constant MCD_PAUSE_PROXY = 0xBE8E3e3618f7474F8cB1d074A26afFef007E98FB;
+
+    address internal constant POOL = 0xC13e21B648A5Ee794902342038FF3aDAB66BE987;
+    address internal constant POOL_ADDRESS_PROVIDER = 0x02C3eA4e34C0cBd694D2adFa2c690EECbC1793eE;
+
+    address internal constant RETH = 0x553303d460EE0afB37EdFf9bE42922D8FF63220e;
+    address internal constant RETH_PRICE_FEED = 0x553303d460EE0afB37EdFf9bE42922D8FF63220e;
+
+    function setUp() public {
+        vm.createSelectFork(vm.rpcUrl('mainnet'), 17336776);
+        _selectPayloadExecutor(MCD_PAUSE_PROXY);
+
+        payload = new AaveV3EthNewListings_20230321();
+    }
+
+    function testPoolActivation() public {
+        createConfigurationSnapshot(
+            'pre-Spark-Ethereum-rETH-Listing',
+            POOL
+        );
+
+        _executePayload(address(payload));
+
+        ReserveConfig[] memory allConfigs = _getReservesConfigs(POOL);
+
+        // rETH
+
+        ReserveConfig memory reth = ReserveConfig({
+            symbol: 'rETH',
+            underlying: RETH,
+            aToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            variableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            stableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            decimals: 18,
+            ltv: 68_50,
+            liquidationThreshold: 79_50,
+            liquidationBonus: 10700,
+            liquidationProtocolFee: 1000,
+            reserveFactor: 1500,
+            usageAsCollateralEnabled: true,
+            borrowingEnabled: true,
+            interestRateStrategy: _findReserveConfigBySymbol(allConfigs, 'rETH').interestRateStrategy,
+            stableBorrowRateEnabled: false,
+            isActive: true,
+            isFrozen: false,
+            isSiloed: false,
+            isBorrowableInIsolation: false,
+            isFlashloanable: true,
+            supplyCap: 20_000,
+            borrowCap: 2_400,
+            debtCeiling: 0,
+            eModeCategory: 1
+        });
+
+        _validateReserveConfig(reth, allConfigs);
+
+        _validateInterestRateStrategy(
+            reth.interestRateStrategy,
+            reth.interestRateStrategy,
+            InterestStrategyValues({
+                addressesProvider: POOL_ADDRESS_PROVIDER,
+                optimalUsageRatio: 45 * (RAY / 100),
+                optimalStableToTotalDebtRatio: 0,
+                baseStableBorrowRate: 0,
+                stableRateSlope1: 0,
+                stableRateSlope2: 0,
+                baseVariableBorrowRate: 0,
+                variableRateSlope1: 7 * (RAY / 100),
+                variableRateSlope2: 300 * (RAY / 100)
+            })
+        );
+
+        _validateAssetSourceOnOracle(
+            POOL_ADDRESS_PROVIDER,
+            RETH,
+            payload.RETH_PRICE_FEED()
+        );
+
+        createConfigurationSnapshot(
+            'post-Spark-Ethereum-rETH-Listing',
+            POOL
+        );
+
+        diffReports(
+            'pre-Spark-Ethereum-rETH-Listing',
+            'post-Spark-Ethereum-rETH-Listing'
+        );
+    }
+}
