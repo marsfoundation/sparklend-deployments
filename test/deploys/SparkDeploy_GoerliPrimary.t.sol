dfd23f73aa1c063851341b9a0d6b5718b566db01
Sam MacPherson <xylom44@gmail.com>
2023-06-12 08:41:44 +0200

    add goerli deploy test

---
+++ test/deploys/SparkDeploy_GoerliPrimary.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import "../SparkDeployBase.t.sol";
+
+// A forked mainnet on Tenderly to test
+contract SparkDeploy_GoerliPrimaryTest is SparkDeployBaseTest {
+
+    constructor() {
+        rpcUrl = getChain("goerli").rpcUrl;
+        forkBlock = 8612863;
+        instanceId = "primary";
+        initialReserveCount = 6;
+    }
+
+}
