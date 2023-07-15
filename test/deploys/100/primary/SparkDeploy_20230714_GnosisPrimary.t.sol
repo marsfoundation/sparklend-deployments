a7efa183a2ed6db1a2f960487a49b02011316177
Sam MacPherson <xylom44@gmail.com>
2023-07-15 11:54:27 +0200

    case consistency

--- test/deploys/100/primary/SparkDeploy_20230714_GnosisPrimary.t.sol
+++ test/deploys/100/primary/SparkDeploy_20230714_GnosisPrimary.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "../../../SparkDeployBase.t.sol";
 
 contract SparkDeploy_20230714_GnosisPrimaryTest is SparkDeployBaseTest {
+    
     constructor() {
         rpcUrl     = getChain("gnosis_chain").rpcUrl;
         forkBlock  = 28941997;
--- validate-deploy.sh
+++ validate-deploy.sh
@@ -9,10 +9,10 @@ if [ -z $1 ]; then
   exit 1
 fi
 
-# load the json file
+# Load the json file
 json=$(cat $1)
 
-# setup variables based on environment
+# Setup variables based on environment
 chainid=$(echo "${json}" | jq -r '.chain')
 verifier="etherscan"
 verifier_url="https://api.etherscan.io/"
@@ -20,28 +20,28 @@ if [ "$2" == "--local" ]; then
   verifier="sourcify"
   verifier_url="http://127.0.0.1:5555/"
 else
-    # special case for gnosisscan
+    # Special case for gnosisscan
     if [ "$chainid" -eq 100 ]; then
         verifier_url="https://api.gnosisscan.io/api"
         export ETHERSCAN_API_KEY=$GNOSISSCAN_API_KEY
     fi
 fi
 
-# iterate over libraries and build the --libraries string
+# Iterate over libraries and build the --libraries string
 libraries=""
 for row in $(echo "${json}" | jq -r '.libraries[]'); do
-  # separate each row into parts
+  # Separate each row into parts
   IFS=':' read -ra parts <<< "$row"
-  # concatenate parts to form the string
+  # Concatenate parts to form the string
   libraries+="--libraries ${parts[0]}:${parts[1]}:${parts[2]} "
 done
 
-# iterate over transactions and filter by transactionType = "CREATE"
+# Iterate over transactions and filter by transactionType = "CREATE"
 for row in $(echo "${json}" | jq -r -c '.transactions[] | select(.transactionType=="CREATE") | {contractAddress, contractName}'); do
-  # extract contractAddress and contractName
+  # Extract contractAddress and contractName
   contractAddress=$(echo "$row" | jq -r '.contractAddress')
   contractName=$(echo "$row" | jq -r '.contractName')
-  # run the verification command
+  # Run the verification command
   forge verify-contract ${contractAddress} ${contractName} ${libraries} --chain-id $chainid --watch --verifier $verifier --verifier-url $verifier_url
   if [ $? -ne 0 ]; then
     echo "Contract verification failed for ${contractName} at ${contractAddress}"
