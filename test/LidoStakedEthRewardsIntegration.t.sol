d197343c88fc5f0ebc244ec5f58aa38c0a9b2839
Sam MacPherson <xylom44@gmail.com>
2023-12-23 10:11:46 -0500

    formatting/comments

--- test/LidoStakedEthRewardsIntegration.t.sol
+++ test/LidoStakedEthRewardsIntegration.t.sol
@@ -6,7 +6,6 @@ import "forge-std/Test.sol";
 import { IAaveOracle }         from "aave-v3-core/contracts/interfaces/IAaveOracle.sol";
 import { IERC20 }              from "aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 import { IPoolDataProvider }   from "aave-v3-core/contracts/interfaces/IPoolDataProvider.sol";
-import { IScaledBalanceToken } from "aave-v3-core/contracts/interfaces/IScaledBalanceToken.sol";
 
 import { IEACAggregatorProxy }         from "aave-v3-periphery/misc/interfaces/IEACAggregatorProxy.sol";
 import { IEmissionManager }            from "aave-v3-periphery/rewards/interfaces/IEmissionManager.sol";
@@ -16,9 +15,9 @@ import { RewardsDataTypes }            from "aave-v3-periphery/rewards/libraries
 
 contract LidoStakedEthRewardsIntegrationTest is Test {
 
-    IAaveOracle        aaveOracle       = IAaveOracle(       0x8105f69D9C41644c6A0803fDA7D03Aa70996cFD9);
-    IPoolDataProvider  poolDataProvider = IPoolDataProvider( 0xFc21d6d146E6086B8359705C8b28512a983db0cb);
-    IEmissionManager   emissionManager  = IEmissionManager(  0xf09e48dd4CA8e76F63a57ADd428bB06fee7932a4);
+    IAaveOracle        aaveOracle       = IAaveOracle(0x8105f69D9C41644c6A0803fDA7D03Aa70996cFD9);
+    IPoolDataProvider  poolDataProvider = IPoolDataProvider(0xFc21d6d146E6086B8359705C8b28512a983db0cb);
+    IEmissionManager   emissionManager  = IEmissionManager(0xf09e48dd4CA8e76F63a57ADd428bB06fee7932a4);
     IRewardsController incentives       = IRewardsController(0x4370D3b6C9588E02ce9D22e684387859c7Ff5b34);
 
     address admin    = 0x3300f198988e4C9C63F75dF86De36421f06af8c4;  // SubDAO Proxy
@@ -32,11 +31,11 @@ contract LidoStakedEthRewardsIntegrationTest is Test {
 
     PullRewardsTransferStrategy transferStrategy;
 
-    address whale1 = 0xf8dE75c7B95edB6f1E639751318f117663021Cf0;  // 7-siblings wallet
-    address whale2 = 0xAA1582084c4f588eF9BE86F5eA1a919F86A3eE57;  // Another whale
+    address whale1 = 0xf8dE75c7B95edB6f1E639751318f117663021Cf0;
+    address whale2 = 0xAA1582084c4f588eF9BE86F5eA1a919F86A3eE57;
 
     function setUp() public {
-        vm.createSelectFork(getChain("mainnet").rpcUrl, 18_707_715);  // Dec 3rd, 2023
+        vm.createSelectFork(getChain("mainnet").rpcUrl, 18707715);  // Dec 3rd, 2023
 
         transferStrategy = new PullRewardsTransferStrategy(
             address(incentives),
@@ -121,7 +120,7 @@ contract LidoStakedEthRewardsIntegrationTest is Test {
         uint256 skipAmount = DURATION / 2;  // 50% of rewards distributed
         skip(skipAmount);
 
-        // 7-siblings wallet should get about half of the rewards at this time
+        // whale1 wallet should get about half of the rewards at this time
         // 79k ETH deposit out of 157k total supplied
         vm.prank(whale1);
         incentives.claimAllRewards(assets, claimAddress);
@@ -131,7 +130,7 @@ contract LidoStakedEthRewardsIntegrationTest is Test {
 
         skip(DURATION - skipAmount);
 
-        // 7-siblings wallet should get about 50% of the total rewards ~10 wstETH
+        // whale1 wallet should get about 50% of the total rewards ~10 wstETH
         vm.prank(whale1);
         incentives.claimAllRewards(assets, claimAddress);
         uint256 whaleReward2 = 10.057468271224958300 ether;
@@ -141,7 +140,7 @@ contract LidoStakedEthRewardsIntegrationTest is Test {
 
         skip(DURATION);  // Skip twice the rewards period
 
-        // 7-siblings should receive no more rewards
+        // whale1 should receive no more rewards
         vm.prank(whale1);
         incentives.claimAllRewards(assets, claimAddress);
         assertEq(IERC20(WSTETH).balanceOf(claimAddress), whaleReward2);
