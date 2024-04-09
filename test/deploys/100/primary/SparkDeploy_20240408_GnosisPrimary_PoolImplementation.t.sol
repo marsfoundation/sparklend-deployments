4f31279eff6073f49230914d4cc1df9b19c4185a
Sam MacPherson <xylom44@gmail.com>
2024-04-09 15:26:23 +0900

    use first deploy and add testing

---
+++ test/deploys/100/primary/SparkDeploy_20240408_GnosisPrimary_PoolImplementation.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import "../../../SparkDeployPoolImplementationBase.t.sol";
+
+contract SparkDeploy_20240408_GnosisPrimary_PoolImplementationTest is SparkDeployPoolImplementationBaseTest {
+    
+    constructor() {
+        rpcUrl      = getChain("gnosis_chain").rpcUrl;
+        forkBlock   = 33350836;
+        instanceId  = "primary";
+        revisionNum = 3;
+    }
+
+}
